507b73e36b7072d2b4a473a0616bcdd4d8a1fbc2
IamCathal <cathalsocallaghan@gmail.com>
2022-11-20 13:59:28 +0000

    Add back webcam temp

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -9,14 +9,15 @@ function webcam() {
   .then(stream => {
     document.getElementById("vidOutput").srcObject = stream;
   });
-  
-  document.getElementById("screenshotButton").addEventListener("click", (ev) => {
-    const context = document.getElementById("canvas").getContext("2d");
-    context.drawImage(document.getElementById("vidOutput"), 0, 0, 200, 140);
-    const data = document.getElementById("canvas").toDataURL("image/png");
-  })
 }
 
+document.getElementById("screenshotButton").addEventListener("click", (ev) => {
+  const context = document.getElementById("canvas").getContext("2d");
+  context.drawImage(document.getElementById("vidOutput"), 0, 0, 200, 140);
+  const data = document.getElementById("canvas").toDataURL("image/png");
+})
+
+webcam()
 giveSwayaaangBordersToItems()
 
 document.getElementById("searchButton").addEventListener("click", (ev) => {
